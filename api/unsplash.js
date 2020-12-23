b5121672da83ec4b99d14948c9b4ab3122d8b87f
Arthur Medeiros <rthurmedeiros@gmail.com>
2020-12-23 00:23:55 -0300

    Lane based unsplash image listing

---
+++ api/unsplash.js
@@ -0,0 +1,5 @@
+import { createApi } from 'unsplash-js'
+
+export const unsplash = createApi({
+  apiUrl: 'http://localhost:8080'
+})
--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,96 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+  <v-row class="flex-row">
+    <v-col
+      v-for="(lane, lk) in lanes"
+      :key="lk"
+      :cols="12 / cols"
+    >
+      <v-row>
+        <v-col
+          v-for="i in lane"
+          :key="i.id"
+        >
+          <v-card>
+            <v-img
+              :src="i.urls.small"
+              :alt="i.alt_description"
+            />
+            <v-list-item two-line>
+              <v-list-item-avatar>
+                <v-avatar>
+                  <v-img :src="i.user.profile_image.small" />
+                </v-avatar>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ i.user.name }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-col>
   </v-row>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import { unsplash } from '~/api/unsplash'
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data () {
+    return {
+      list: [],
+      lanes: [],
+      cols: 1,
+      page: 0,
+      perPage: 48
+    }
+  },
+  watch: {
+    '$vuetify.breakpoint.name': 'buildLanes'
+  },
+  mounted () {
+    this.fetch()
+  },
+  methods: {
+    fetch () {
+      unsplash.photos.list({
+        page: this.page,
+        perPage: this.perPage
+      })
+        .then((e) => {
+          this.list = e.response.results
+          this.buildLanes()
+        })
+    },
+    buildLanes () {
+      let cols = 1
+      let col = 0
+
+      if (this.$vuetify.breakpoint.xl) {
+        cols = 4
+      } else if (this.$vuetify.breakpoint.mdAndUp) {
+        cols = 3
+      }
+
+      const lanes = []
+
+      this.list.forEach((i) => {
+        if (!lanes[col]) {
+          lanes[col] = []
+        }
+        lanes[col].push(i)
+
+        col += 1
+        if (col >= cols) {
+          col = 0
+        }
+      })
+
+      this.lanes = lanes
+      this.cols = cols
+    }
   }
 }
 </script>
